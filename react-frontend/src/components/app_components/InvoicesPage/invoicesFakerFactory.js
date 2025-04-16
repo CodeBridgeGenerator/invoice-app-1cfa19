9719527faf799b019d41e615bc8c0a67e12ef9c7
Code Bridge Machine <ahmed@codebridge.my>
2025-04-16 17:40:02 +0800

    CodeBridge commit

---
+++ react-frontend/src/components/app_components/InvoicesPage/invoicesFakerFactory.js
@@ -0,0 +1,20 @@
+
+import { faker } from "@faker-js/faker";
+export default (user,count,companyIdIds,itemIdIds) => {
+    let data = [];
+    for (let i = 0; i < count; i++) {
+        const fake = {
+companyId: companyIdIds[i % companyIdIds.length],
+itemId: itemIdIds[i % itemIdIds.length],
+quantity: faker.datatype.number(""),
+subTotal: faker.datatype.number(""),
+discount: faker.datatype.number(""),
+total: faker.datatype.number(""),
+
+updatedBy: user._id,
+createdBy: user._id
+        };
+        data = [...data, fake];
+    }
+    return data;
+};
